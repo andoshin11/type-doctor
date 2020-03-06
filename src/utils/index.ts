43af5709b41b0c90274275d1c2c9bff09c2eb564
andoshin11 <shinglish11@gmail.com>
2020-03-07 05:07:02 +0900

    Enhance utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -13,3 +13,15 @@ export function mutable<T>(arg: readonly T[]): T[] {
 export function flatten<T>(data: T[][]): T[] {
   return data.reduce((acc, ac) => [...acc, ...ac], [] as T[])
 }
+
+// filter items of a which does not exist in b
+export function diff<T>(a: T[], b: T[]): T[] {
+  return a.filter(item => !b.includes(item))
+}
+
+export const pluck = <T extends object, K extends keyof T>(data: T[], key: K): T[K][] => data.map(item => item[key])
+
+export const uniqBy = <T extends object>(arr: T[], key: keyof T) => {
+  const propList = pluck(arr, key)
+  return arr.filter((elm, i, self) => propList.indexOf(elm[key]) === i)
+}
