0b3452d72e264bf83d10e6d91618ab006a73eca4
andoshin11 <shinglish11@gmail.com>
2020-03-07 05:06:45 +0900

    Add FileEntry type

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,5 @@ export * from './symptoms'
 export type Location = { line: number; character: number }
 
 export type DiagnosticWithRange = Omit<ts.Diagnostic, 'start' | 'length'> & { start: NonNullable<ts.Diagnostic['start']>; length: NonNullable<ts.Diagnostic['length']> }
+
+export type FileEntry = Map<string, { version: number; scriptSnapshot: ts.IScriptSnapshot }>
