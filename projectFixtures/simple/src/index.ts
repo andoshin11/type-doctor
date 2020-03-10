ac1ed779fc0e4941a6d4fadd8af6728fd37bca50
andoshin11 <shinglish11@gmail.com>
2020-03-10 09:05:54 +0900

    Upadte projectFixture

--- projectFixtures/simple/src/index.ts
+++ projectFixtures/simple/src/index.ts
@@ -1,5 +1,13 @@
+// import * as https from 'https' <- this import statement will crash the compiler somehow :thinking_face:
+
 function say(input: number) {
   console.log(input)
 }
 
-say('hello')
+say1('hello')
+
+const hoge = 'hoge'
+hoge = 'fuga'
+
+const fuga = 'fuga'
+fuga = 'hoge'
